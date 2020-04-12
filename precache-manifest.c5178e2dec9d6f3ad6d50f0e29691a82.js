ba36eddfd7233d9203473a919b9c4a94876323a5
Aleksandr Biryukov <missliss1@yandex.ru>
2020-04-13 02:38:50 +0300

    first

---
+++ precache-manifest.c5178e2dec9d6f3ad6d50f0e29691a82.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d17a58d8d37b56f8cccffa5a69d7f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "849ad542974add86c725",
+    "url": "/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "f7c77b14992efb02cebe",
+    "url": "/static/css/main.ddca0a9e.chunk.css"
+  },
+  {
+    "revision": "849ad542974add86c725",
+    "url": "/static/js/2.c37481a8.chunk.js"
+  },
+  {
+    "revision": "280b48dbc3281f342da16f66e41cf46c",
+    "url": "/static/js/2.c37481a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c77b14992efb02cebe",
+    "url": "/static/js/main.1138915c.chunk.js"
+  },
+  {
+    "revision": "22ecc13d32696d0ac9e3",
+    "url": "/static/js/runtime-main.cc0ae68c.js"
+  },
+  {
+    "revision": "76b65dbca0d71e87332f19cc6c7caec6",
+    "url": "/static/media/android.76b65dbc.png"
+  },
+  {
+    "revision": "69c1f9aba49ee025db4109549132c526",
+    "url": "/static/media/iphone.69c1f9ab.png"
+  },
+  {
+    "revision": "25290d3ebabba60a56e7164e92d7cdd2",
+    "url": "/static/media/loader.25290d3e.gif"
+  }
+]);
